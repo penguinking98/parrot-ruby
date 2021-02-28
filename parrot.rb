9f600d6686574e86621f70cc92db1150ac36e425
Ben Kramer <benkramer614@gmail.com>
2021-02-28 18:36:01 -0500

    commit tree 2a3562a2d522281ff2d7882351b74d66b1af8ce8

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,18 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(msg = nil)
+    # msg ||= "Squawk!"
+    # msg = msg || "Squawk!"
+    #if msg == nil
+    #    msg = "Squawk!"
+    #end
+    #if ! msg
+    #    msg = "Squawk!"
+    #end
+    msg = msg ? msg : "Squawk!"
+    puts msg
+    msg
+ 
+end
+
+  
